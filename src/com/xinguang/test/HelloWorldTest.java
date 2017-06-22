65c511128fa68b394247e28de30540cb65044edf
lisi111222 <1021604815@qq.com>
2017-06-22 15:43:32 +0800

    update 03

--- src/com/xinguang/test/HelloWorldTest.java
+++ src/com/xinguang/test/HelloWorldTest.java
@@ -4,5 +4,6 @@ public class HelloWorldTest {
 	
 	public static void main(String args[]){
 		System.out.println("Hello Mile,Nice to meet you!");
+		System.out.println("Nice to meet you too!");
 	}
 }
